3b67cf62b5d4a5e45805820aa258fef01f834c23
alexander perlov <perlov3301@gmail.com>
2023-10-24 05:42:15 +0300

    upvotes update

--- src/server.js
+++ src/server.js
@@ -7,35 +7,30 @@ app.use(express.json());
 //download data: get name_parameter->get info from db
 app.get('/api/articles/:name', async (req,res)=> {
    const { name } = req.params;
-   // listening at localhost to port 27017.
-  // from mongodb.com: const db = connect("localhost:27017/react-blog-db");
-   //we do need actually IP=127.0.0.1
    const client = new MongoClient('mongodb://127.0.0.1:27017');
    await client.connect();
-   // in mongosh: use react-blog-db
    const db1 = client.db('react-blog-db');
-   // in mongosh: db.articles.findOne({name:name});
+   // mongosh:db.articles.find({name:name});
    const article = await db1.collection('articles').findOne({ name:name });
-  // if (name) {
-  //   const article = await db1.collection('articles').findOne({ name });
-  // } else {res.send("article din't match");}
-  //  res.send(article);
-   if (article) {
-    res.json(article);
-   } else {
-    res.sendStatus(404);
-    // res.status(404).send("...")
+ 
+   if (article) {   res.json(article);
+   } else {   res.sendStatus(404); // res.status(404).send("...")
    }
-  // res.json(article);
 });
 // define upvote end point put(path,(req,res)=>{})
-app.put('/api/articles/:name/upvote', (req,res)=>{
-//    const name = req.params.name;
-   const { name } = req.params;
-   const article = articlesInfo.find(item=> item.name===name);
+app.put('/api/articles/:name/upvote', async (req,res)=>{ 
+  const { name } = req.params;
+  const client = new MongoClient('mongodb://127.0.0.1:27017');
+   await client.connect();
+   const db1 = client.db('react-blog-db');
+   // set=100: $set: {upvotes:100} // increment upvotes by 1
+   await db1.collection('articles').updateOne(
+       { name: name },
+       { $inc: { upvotes: 1 }, }
+     );
+   const article = await db1.collection('articles').findOne({ name });
    if (article) { 
-    article.upvotes += 1; 
-    const msg1=`upvotes for the article ${name} has grown to ${article.upvotes}  `;
+    const msg1=`the ${name} article now has ${article.upvotes} upvotes `;
     res.send(msg1);
  }  else {
     res.send(`the article \"${name}\" does\'nt exist`);
@@ -44,10 +39,8 @@ app.put('/api/articles/:name/upvote', (req,res)=>{
 });
 app.post('/api/articles/:name/comments',(req,res)=>{
   const { name } = req.params;
- // console.log("server;req.params: ",req.params);
-  //  const postedBy = req.body.postedBy;
-  //  const text = req.body.text;
   const { postedBy, text } =req.body;
+  
   const article=articlesInfo.find(item=> item.name === name);
   if (article) {
     article.comments.push({ postedBy, text });
